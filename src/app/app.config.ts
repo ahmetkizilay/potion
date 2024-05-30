c7afb08ef437f04d60e2f194bc64c50db8f14112
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-05-29 23:09:46 -0400

    init angular firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "potion-bce28",
+      "appId": "1:91139147310:web:407b590998cdd83dbf01bf",
+      "storageBucket": "potion-bce28.appspot.com",
+      "apiKey": "AIzaSyAWqj_zYk5aKnhcAG2n_eoL_7dpJZLnxQY",
+      "authDomain": "potion-bce28.firebaseapp.com",
+      "messagingSenderId": "91139147310",
+      "measurementId": "G-X92K3JKDK1"
+    })),
+    provideAuth(() => getAuth()),
+  ]
 };
