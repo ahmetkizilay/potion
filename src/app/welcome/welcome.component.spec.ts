7ea0e806495fdd2172e3637894667e33e90bf87e
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-02 10:58:06 -0400

    initial save action for input text

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -2,20 +2,28 @@ import { TestBed, waitForAsync } from '@angular/core/testing';
 import { WelcomeComponent } from './welcome.component';
 import { UserService } from '../user/user.service';
 import { Observable, of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { WelcomeService } from './welcome.service';
 
 class MockUserService {
   user$: Observable<any | null> = of(null);
-  logout() {}
+  logout() { }
+}
+class MockWelcomeService {
+  save(_: string) { }
 }
 
 describe('WelcomeComponent', () => {
   let userService: MockUserService;
+  let welcomeService: MockWelcomeService;
   beforeEach(waitForAsync(() => {
     userService = new MockUserService();
+    welcomeService = new MockWelcomeService();
     TestBed.configureTestingModule({
       imports: [WelcomeComponent],
       providers: [
         { provide: UserService, useValue: userService },
+        { provide: WelcomeService, useValue: welcomeService },
       ],
     }).compileComponents();
   }));
@@ -26,7 +34,7 @@ describe('WelcomeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('displays the user email', () => {    
+  it('displays the user email', () => {
     userService.user$ = of({ email: 'email' });
     const fixture = TestBed.createComponent(WelcomeComponent);
     const component = fixture.componentInstance;
@@ -35,7 +43,7 @@ describe('WelcomeComponent', () => {
     });
   });
 
-  it('displays the null user', () => {    
+  it('displays the null user', () => {
     userService.user$ = of(null);
     const fixture = TestBed.createComponent(WelcomeComponent);
     const component = fixture.componentInstance;
@@ -45,11 +53,37 @@ describe('WelcomeComponent', () => {
   });
 
   it('logs out the user', () => {
+    spyOn(userService, 'logout');
+
     const fixture = TestBed.createComponent(WelcomeComponent);
     const component = fixture.componentInstance;
-    const userService = TestBed.inject(UserService);
-    spyOn(userService, 'logout');
-    component.logout();
+
+    const btnLogout = fixture.debugElement.query(By.css('button#btn-logout'));
+    btnLogout.triggerEventHandler('click', null);
+
     expect(userService.logout).toHaveBeenCalled();
+  });
+
+  it('contains a textContent element', () => {
+    const fixture = TestBed.createComponent(WelcomeComponent);
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.textContainer).toBeTruthy();
+  });
+
+  it('calls save', () => {
+    spyOn(welcomeService, 'save');
+
+    const fixture = TestBed.createComponent(WelcomeComponent);
+    const component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    component.textContainer.nativeElement.innerText = 'Lorem ipsum dolor sit amet';
+
+    const btnSave = fixture.debugElement.query(By.css('button#btn-save'));
+    btnSave.triggerEventHandler('click', null);
+
+    expect(welcomeService.save).toHaveBeenCalledWith('Lorem ipsum dolor sit amet');
+
   });
 });
