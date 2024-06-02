47321be4e095b8e9f97d48c046cfce658f103d2e
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-02 10:58:06 -0400

    initial save action for input text

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject} from '@angular/core';
+import { Component, ElementRef, ViewChild, inject} from '@angular/core';
 import { Observable, filter, first, lastValueFrom, map } from 'rxjs';
 import { UserService } from '../user/user.service';
 import { AsyncPipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { WelcomeService } from './welcome.service';
 
 @Component({
   standalone: true,
@@ -11,14 +12,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./welcome.component.css'],
   imports: [AsyncPipe]
 })
-export class WelcomeComponent {  
+export class WelcomeComponent {
 	private router: Router = inject(Router);
   private userService: UserService = inject(UserService);
+  private welcomeService: WelcomeService = inject(WelcomeService);
+  @ViewChild('text') textContainer!: ElementRef<HTMLDivElement>;
+
   username$: Observable<string> = this.userService.user$.pipe(map(user => user?.email ?? 'Anonymous'));
 
   async logout() {
     await this.userService.logout();
     await lastValueFrom(this.userService.isSignedIn$.pipe(filter(val => !val), first()));
 		return this.router.navigate(['/logout']);
+  }
+
+  save() {
+    const text = this.textContainer.nativeElement.innerText;
+    this.welcomeService.save(text);
   }
 }
