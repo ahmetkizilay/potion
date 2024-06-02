a26e050150348079b979a0526202fcac653a3820
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-02 14:01:41 -0400

    save to firestore

--- src/app/welcome/welcome.service.ts
+++ src/app/welcome/welcome.service.ts
@@ -1,10 +1,27 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
+import { Firestore, addDoc, CollectionReference, collection } from '@angular/fire/firestore';
+
+export interface MorningPage {
+  text: string;
+}
 
 @Injectable(
   { providedIn: 'root' }
 )
 export class WelcomeService {
-  save(text: string) {
-    console.log('Saving text:', text)
+  private firestore = inject(Firestore);
+  morningPageCollection: CollectionReference;
+
+  constructor() {
+    this.morningPageCollection = collection(this.firestore, 'morningPages');
+  }
+  async save(text: string) {
+    console.log('Saving text:', text);
+    try {
+      const docReference = await addDoc(this.morningPageCollection, <MorningPage> { text });
+      console.log(docReference.id);
+    } catch (e) {
+      console.error('Error saving text:', e);
+    }
   }
 }
