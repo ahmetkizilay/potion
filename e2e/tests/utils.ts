aba9fdc7fe96b009a02b1cc4b76494b0ab8edff2
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-05 20:48:14 -0400

    add e2e test for login action

---
+++ e2e/tests/utils.ts
@@ -0,0 +1,47 @@
+
+import * as puppeteer from 'puppeteer';
+
+const baseUrl = process.env['baseUrl'] ?? 'http://localhost:4200/';
+let browser: puppeteer.Browser;
+let page: puppeteer.Page;
+
+export function setupBrowserHooks(path = ''): void {
+
+  beforeAll(async () => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+    browser = await puppeteer.launch({headless: true, devtools: false});
+  });
+
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto(`${baseUrl}${path}`);
+  });
+
+  afterEach(async () => {
+    await page?.close();
+  });
+
+
+  afterAll(async () => {
+    await browser?.close();
+  });
+
+}
+
+export function getBrowserState(): {
+  browser: puppeteer.Browser;
+  page: puppeteer.Page;
+  baseUrl: string;
+} {
+  if (!browser) {
+    throw new Error(
+      'No browser state found! Ensure `setupBrowserHooks()` is called.'
+    );
+  }
+  return {
+    browser,
+    page,
+    baseUrl,
+  };
+}
