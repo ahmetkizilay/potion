c7071d70d63d31ea89f0f8f40500fab52950bc30
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-09 22:27:27 -0400

    clear APP_CONFIG data in functions testing

--- functions/src/functions.spec.ts
+++ functions/src/functions.spec.ts
@@ -4,13 +4,13 @@ import { connectAuthEmulator, getAuth, createUserWithEmailAndPassword, Auth } fr
 import { connectFirestoreEmulator, doc, getDoc, Firestore, getFirestore } from 'firebase/firestore';
 
 const APP_CONFIG = {
-  'projectId': 'potion-bce28',
-  'appId': '1:91139147310:web:407b590998cdd83dbf01bf',
-  'storageBucket': 'potion-bce28.appspot.com',
-  'apiKey': 'AIzaSyAWqj_zYk5aKnhcAG2n_eoL_7dpJZLnxQY',
-  'authDomain': 'potion-bce28.firebaseapp.com',
-  'messagingSenderId': '91139147310',
-  'measurementId': 'G-X92K3JKDK1',
+  'projectId': '',
+  'appId': '',
+  'storageBucket': '',
+  'apiKey': '',
+  'authDomain': '',
+  'messagingSenderId': '',
+  'measurementId': '',
 };
 
 describe('Firebase Functions', () => {
