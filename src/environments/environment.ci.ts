795efb49a142929d99be9f78871eb31bbe62face
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-13 23:17:12 -0400

    copy firebase config values into the ci environment

--- src/environments/environment.ci.ts
+++ src/environments/environment.ci.ts
@@ -5,6 +5,12 @@ export const environment = {
     config: {
       "projectId": "potion-bce28",
       "appId": "1:91139147310:web:407b590998cdd83dbf01bf",
+      "storageBucket": "potion-bce28.appspot.com",
+      "apiKey": "AIzaSyAWqj_zYk5aKnhcAG2n_eoL_7dpJZLnxQY",
+      "authDomain": "potion-bce28.firebaseapp.com",
+      "messagingSenderId": "91139147310",
+      "measurementId": "G-X92K3JKDK1",
+      "recaptchaSiteKey": "6LeLKfgpAAAAAMikMW1e4zvlVtBzwa1Asze9jNJC",
     },
     emulators: {
       auth: true,
