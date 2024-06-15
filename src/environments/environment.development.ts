fbd2382b4a114850ebf24e5daa65aaeb3f62d626
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-06-15 02:56:47 -0400

    setup app check

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,12 +3,13 @@ export const environment = {
   firebase: {
     config: {
       "projectId": "potion-bce28",
-      "appId": "1:91139147310:web:407b590998cdd83dbf01bf",
+      "appId": "1:91139147310:web:86a216a4228c0e8cbf01bf",
       "storageBucket": "potion-bce28.appspot.com",
-      "apiKey": "AIzaSyAWqj_zYk5aKnhcAG2n_eoL_7dpJZLnxQY",
+      "apiKey": "AIzaSyAKiycQ8NOOehc3LsHE6Y6OElPGjvXbe84",
       "authDomain": "potion-bce28.firebaseapp.com",
       "messagingSenderId": "91139147310",
-      "measurementId": "G-X92K3JKDK1"
+      "measurementId": "G-CKGBLC22Y7",
+      "recaptchaSiteKey": "6LfEP_gpAAAAABahtqQRf9naBZEAcI12mRK28YW4",
     },
     emulators: {
       auth: true,
