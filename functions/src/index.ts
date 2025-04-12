79a012efea6068a048a041cebdae57533fca5c01
ahmet.kizilay <ahmet.kizilay@gmail.com>
2025-04-12 16:16:26 +0000

    fix linter errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
-import * as logger from 'firebase-functions/logger'
+import * as logger from 'firebase-functions/logger';
 import * as admin from 'firebase-admin';
 import * as Firestore from 'firebase-admin/firestore';
-import { beforeUserCreated} from 'firebase-functions/identity'
+import { beforeUserCreated } from 'firebase-functions/identity';
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -9,7 +9,7 @@ const db = admin.firestore();
 // Create a user doc in Firestore in the users collection
 export const createUserDoc =
   beforeUserCreated(async (event) => {
-    logger.info('Received beforeUserCreated event: ', event.eventId)
+    logger.info('Received beforeUserCreated event: ', event.eventId);
     if (!event.data) {
       throw new Error('No event data');
     }
@@ -17,7 +17,7 @@ export const createUserDoc =
     if (!userEmail) {
       throw new Error('No user email');
     }
-    logger.info('Creating user doc for user: ', event.eventId)
+    logger.info('Creating user doc for user: ', event.eventId);
 
     await db.collection('users').doc(event.eventId).set({
       email: userEmail,
