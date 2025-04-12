5dd010181b735cae14e2a8bf9d46c247ad136259
ahmet.kizilay <ahmet.kizilay@gmail.com>
2025-04-12 16:02:22 +0000

    update function handler before user created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,27 @@
-import * as functions from 'firebase-functions';
-import * as logger from 'firebase-functions/logger';
+import * as logger from 'firebase-functions/logger'
 import * as admin from 'firebase-admin';
 import * as Firestore from 'firebase-admin/firestore';
+import { beforeUserCreated} from 'firebase-functions/identity'
 
 admin.initializeApp();
 const db = admin.firestore();
 
 // Create a user doc in Firestore in the users collection
 export const createUserDoc =
-  functions.auth.user().beforeCreate(async (user) => {
-    logger.info('Creating user doc for user: ', user.uid);
-    await db.collection('users').doc(user.uid).set({
-      email: user.email,
+  beforeUserCreated(async (event) => {
+    logger.info('Received beforeUserCreated event: ', event.eventId)
+    if (!event.data) {
+      throw new Error('No event data');
+    }
+    const userEmail = event.data.email;
+    if (!userEmail) {
+      throw new Error('No user email');
+    }
+    logger.info('Creating user doc for user: ', event.eventId)
+
+    await db.collection('users').doc(event.eventId).set({
+      email: userEmail,
       createdAt: Firestore.FieldValue.serverTimestamp(),
     });
     return;
   });
-
-
